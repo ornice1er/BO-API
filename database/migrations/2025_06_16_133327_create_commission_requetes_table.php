4b10116478e831cd961cc9ab2fcc100da237afdc
OrniceDev <ornihouss1@gmail.com>
2025-12-16 12:33:05 +0100

    fix data

--- database/migrations/2025_06_16_133327_create_commission_requetes_table.php
+++ database/migrations/2025_06_16_133327_create_commission_requetes_table.php
@@ -11,16 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::create('commission_requetes', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->foreignId('commission_id')->constrained('commissions')->onDelete('cascade');
-        //     $table->foreignId('requete_id')->constrained('requetes')->onDelete('cascade');
-        //     $table->decimal('global_mark', 5, 2)->nullable();
-        //     $table->string('status')->default('pending');
-        //     $table->timestamps();
+        Schema::create('commission_requetes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('commission_id')->constrained('commissions')->onDelete('cascade');
+            $table->foreignId('requete_id')->constrained('requetes')->onDelete('cascade');
+            $table->decimal('global_mark', 5, 2)->nullable();
+            $table->string('status')->default('pending');
+            $table->timestamps();
             
-        //     $table->unique(['commission_id', 'requete_id']);
-        // });
+            $table->unique(['commission_id', 'requete_id']);
+        });
     }
 
     /**
