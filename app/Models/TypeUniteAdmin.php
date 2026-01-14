2b76d5b5aa8a73dd33385fba82eb5322b5d7dabc
Ornice <ornihouss1@gmail.com>
2026-01-14 20:42:01 +0100

    fix data

--- app/Models/TypeUniteAdmin.php
+++ app/Models/TypeUniteAdmin.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use eloquentFilter\QueryFilter\ModelFilters\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Str;
 
 class TypeUniteAdmin extends Model
 {
@@ -12,4 +13,18 @@ class TypeUniteAdmin extends Model
     use Filterable,HasFactory;
     private static $whiteListFilter = ['*'];
     protected $guarded = [];
+
+
+      /**
+     * Fonction boot pour générer un code unique avant la création
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        // Cette méthode est exécutée avant la création de chaque enregistrement
+        self::creating(function ($model) {
+            $model->code = Str::uuid();
+        });
+    }
 }
