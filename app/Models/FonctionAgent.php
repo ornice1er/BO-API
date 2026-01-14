af8a88249a029c6d617cdaf4dc3f75ca52f6cf52
Ornice <ornihouss1@gmail.com>
2026-01-14 21:07:42 +0100

    fix data

--- app/Models/FonctionAgent.php
+++ app/Models/FonctionAgent.php
@@ -5,9 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use eloquentFilter\QueryFilter\ModelFilters\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Str;
 class FonctionAgent extends Model
 {
     use Filterable,HasFactory;
     private static $whiteListFilter = ['*'];
     protected $guarded = [];
+
+
+       public static function boot()
+    {
+        parent::boot();
+
+        // Cette méthode est exécutée avant la création de chaque enregistrement
+        self::creating(function ($model) {
+            $model->code = Str::uuid();
+        })
+
+    }
 }
