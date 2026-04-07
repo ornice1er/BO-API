7979a46288bd7df09e74249eca2839fdc3b8ae4c
Ornice <ornihouss1@gmail.com>
2026-04-07 15:56:35 +0100

    MAJ des données

--- app/Http/Controllers/DocumentActeController.php
+++ app/Http/Controllers/DocumentActeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\DocumentActe;
+use App\Models\DocumentActeLog;
 use App\Models\DocumentCircuitEtape;
 use App\Models\EtapeDocumentProduit;
 use App\Models\Requete;
