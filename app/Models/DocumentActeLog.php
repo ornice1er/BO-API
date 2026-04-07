47b913e08cfd819a6e407099d146c15cc32a010f
Ornice <ornihouss1@gmail.com>
2026-04-07 15:58:56 +0100

    MAJ des données

---
+++ app/Models/DocumentActeLog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentActeLog extends Model
+{
+     protected $table = 'document_acte_logs';
+    protected $guarded = [];
+ 
+    protected $casts = [];
+}
