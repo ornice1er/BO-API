c31a1e6e34db6340ffcc599af49f0421d1401bff
Ornice <ornihouss1@gmail.com>
2026-04-21 12:51:56 +0100

    MAJ des données

--- app/Http/Repositories/AgendaRepository.php
+++ app/Http/Repositories/AgendaRepository.php
@@ -163,7 +163,7 @@ function getContent($agenda) {
         : 'Non définie';
 
     $prestation = $agenda->requete->prestation->name ?? 'N/A';
-    $auteur     = optional($agenda->user->agent)->lastname . ' ' . optional($agenda->user->agent)->firstname;
+    $auteur     = optional($agenda->user?->name) ?? 'Un agent du service';
 
     $content = "
         Bonjour,
