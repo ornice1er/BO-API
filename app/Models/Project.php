0dccf4bd49a53b679b30eff87d5dd26d86325eea
Ornice <ornihouss1@gmail.com>
2026-04-22 07:39:56 +0100

    MAJ clé de decision

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -16,7 +16,7 @@ class Project extends Model
 
     protected $casts = [
         'request_ids' => 'array',
-        'prestations' => 'array',
+        //'prestations' => 'array',
     ];
 
     /**
