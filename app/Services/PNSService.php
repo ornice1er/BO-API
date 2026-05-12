fcacb08d931acd29e77f5850ce07c6cee7a69a71
Ornice <ornihouss1@gmail.com>
2026-05-12 20:11:18 +0100

    MAJ des données

--- app/Services/PNSService.php
+++ app/Services/PNSService.php
@@ -31,6 +31,7 @@ class PNSService{
                     'header' => $this->header,
                     'payload' => $this->payload
                 ]);
+                info('PNS URI', env('PNS_URI').'/api/portal/event/uxp/rest');
                 $response = Http::withHeaders($this->header)->post(env('PNS_URI').'/api/portal/event/uxp/rest',$this->payload);
                  if ($response?->successful()) {
                     return $response?->json();
